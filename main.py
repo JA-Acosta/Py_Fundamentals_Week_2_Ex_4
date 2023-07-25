9a47896e95e5b3de46a8138fade2ba2d9435b5ad
JAAR <joe.aaron.acosta@outlook.com>
2023-07-24 21:43:24 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/24/2023
+>>> Practicing Fundamentals Program 10
+>>> Version 0
+'''
+
+'''
+>>> Asks the user for a list of numbers then calculates the sum of all even numbers as well as determines which value is the largest.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
